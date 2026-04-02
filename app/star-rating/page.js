129066c470f2d85fab3d043cd67da0a82e92289f
Jitendra Gupta <jiitendra.gupta@outlook.com>
2026-04-02 11:15:32 +0530

    Add Star rating component

--- app/star-rating/page.js
+++ app/star-rating/page.js
@@ -1,14 +1,18 @@
+'use client';
+
+import { Container } from '../common.styles';
 import {StarRating} from './star-rating';
 
 const Page = () => {
   return (
-      <div>
-        <StarRating
-          maxRating={5}
-          size={24}
-          defaultRating={3}
-        />
-      </div>)
+    <Container>
+      <StarRating
+        maxRating={5}
+        size={24}
+        defaultRating={3}
+      />
+    </Container>
+  )
 }
 
 export default Page;
